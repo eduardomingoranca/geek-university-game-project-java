ed169c560782ce22dd6a9937c36726cd8ba2d627
eduardomingoranca <eduardomingoranca@gmail.com>
2022-12-24 15:26:18 -0300

    add testing model

---
+++ game/src/com/brazil/geek/view/Teste.java
@@ -0,0 +1,12 @@
+package com.brazil.geek.view;
+
+import com.brazil.geek.model.Calcular;
+
+public class Teste {
+
+    public static void main(String[] args) {
+        Calcular calcular = new Calcular(5);
+
+        System.out.println(calcular);
+    }
+}
